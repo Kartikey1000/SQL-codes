706eb78321f2916d464bd75ef6dec73be2bdbfb0
kartikey <ks632794@gmail.com>
2026-09-24 10:40:07 +0530

    Create joins.sql

---
+++ joins.sql
@@ -0,0 +1,14 @@
+SELECT *FROM "sea_lions"
+JOIN "migrations" ON "migrations"."id"="sea_lions"."id";
+
+SELECT *FROM "sea_lions"
+LEFT JOIN "migrations" ON "migrations"."id"="sea_lions"."id"; 
+
+SELECT *FROM "sea_lions"
+RIGHT JOIN "migrations" ON "migrations"."id"="sea_lions"."id"; 
+
+SELECT *FROM "sea_lions"
+FULL JOIN "migrations" ON "migrations"."id"="sea_lions"."id"; 
+
+SELECT *FROM "sea_lions"
+NATURAL JOIN "migrations";
