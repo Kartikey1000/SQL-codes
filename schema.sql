0fbc9f4bd96e70af7687efc502af515898569077
kartikey <ks632794@gmail.com>
2026-09-26 17:12:03 +0530

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,25 @@
+CREATE TABLE "cards"(
+    "id" INTEGER,
+    PRIMARY KEY("id")
+);
+
+CREATE TABLE "stations"(
+    "id" INTEGER,
+    "name" TEXT NOT NULL UNIQUE,
+    "line" TEXT NOT NULL,
+    PRIMARY KEY("id")
+);
+
+CREATE TABLE "swipes"(
+    "id" INTEGER,
+    "card_id" INTEGER,
+    "station_id" INTEGER,
+    "type" TEXT NOT NULL CHECK("type" in ('enter','exit','deposit')),
+    "datetime" NUMERIC NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "amount" NUMERIC NOT NULL CHECK("amount" !=0),
+    PRIMARY KEY("id"),
+    --PRIMARY KEY("rider_id","station_id")--
+    FOREIGN KEY("card_id") REFERENCES "cards"("id"),
+    FOREIGN KEY("station_id") REFERENCES "stations"("id")
+
+);
